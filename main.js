29ffcbd3f393b2be0fca78409d6ea83f17676185
John Appiah Abayem <aappiahjohn@gmail.com>
2026-02-26 08:52:01 +0000

    Update profile picture handling in main.js

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
-let images = ["IMG_4241.jpg", "IMG_4423.jpg"];
-let index = 0;
+// Set the initial profile picture//
+document.getElementById("profile-pic").style.backgroundImage = "url('IMG_4241.jpg')";
 
+let images = ["IMG_4241.jpg", "IMG_4423.JPG"];
+let index = 0;
+// Change the profile picture every 5 seconds//
 setInterval(function () {
   index = (index + 1) % images.length;
-  document.getElementById("profile-pic").src = images[index];
+  document.getElementById("profile-pic").style.backgroundImage = "url('" + images[index] + "')";
 }, 5000); // 5 seconds 
-
+// navigation logo//
 document.getElementById("logo").style.backgroundImage = "url('AJ Logo 2.png')";
