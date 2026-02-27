4cad741a07a7ed3d83158adc4a09109f8d9ab92b
John Appiah Abayem <aappiahjohn@gmail.com>
2026-02-27 01:57:13 +0000

    Update profile picture array with new image

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // Set the initial profile picture//
 document.getElementById("profile-pic").style.backgroundImage = "url('IMG_4241.jpg')";
 
-let images = ["IMG_4241.jpg", "IMG_4423.JPG"];
+let images = ["IMG_4241.jpg", "IMG_1703.jpg"];
 let index = 0;
 // Change the profile picture every 5 seconds//
 setInterval(function () {
